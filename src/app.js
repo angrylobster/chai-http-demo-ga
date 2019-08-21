e3a231a640491171182f7b09216abaa3329e945f
Min Shan <min.shan@online-pajak.com>
2019-08-21 10:51:38 +0800

    Refactor files for initial commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const app = express();
 const DEFAULT_PORT = 3000;
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("homepage"));
-
 app.listen(DEFAULT_PORT, () =>
   console.log(`=== SERVER IS LISTENING ON PORT ${DEFAULT_PORT} ===`)
 );
