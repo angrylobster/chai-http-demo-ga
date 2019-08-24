0793d9969f574b29973e9aa593187a0b7976b7d9
Min Shan <min.shan@online-pajak.com>
2019-08-24 14:50:57 +0800

    Refactor config for test env

---
+++ src/config/test/index.js
@@ -0,0 +1,11 @@
+const test = {
+    db: {
+        user: 'shan',
+        password: 'abc123',
+        host: 'localhost',
+        database: 'testdb',
+        port: 5432
+    }
+};
+
+module.exports = test;
--- src/db/postgres/index.js
+++ src/db/postgres/index.js
@@ -1,11 +1,6 @@
 const { Pool } = require('pg');
-const pool = new Pool({
-    user: 'shan',
-    password: 'abc123',
-    host: 'localhost',
-    database: 'testdb',
-    port: 5432
-});
+const { config } = require('../../config');
+const pool = new Pool(config.db);
 
 async function query(queryString = '', params = []) {
     const results = await pool.query(queryString, params);
